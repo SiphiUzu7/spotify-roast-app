008d1cc4bf6c5c2d7051319464367003377074d7
Siphiwe Ngidi <mohlabaisiphiwe7@gmail.com>
2026-02-03 18:20:06 +0200

    Create CopyTag component, import to Landing.jsx

---
+++ src/components/CopyTag.jsx
@@ -0,0 +1,11 @@
+
+
+function CopyTag() {
+    return (
+        <div className="copy-tag">
+            <p>By Logging In, You Agree To Get Roasted 🔥</p>
+         </div>
+    );
+};
+
+export default CopyTag;
