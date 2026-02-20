9bc19287913ec2162a18f93e9e147f59e1bd77ab
Siphiwe Ngidi <mohlabaisiphiwe7@gmail.com>
2026-02-20 19:26:48 +0200

    Fetch Spotify data and call Gemini server

--- src/pages/Fetch.jsx
+++ src/pages/Fetch.jsx
@@ -1,8 +1,57 @@
+import { useEffect } from 'react';
+import {useNavigate } from "react-router-dom";
 import LogoComponent from '../components/Logo.jsx'
 import fetchLoad from "../assets/FetchLoad.gif";
 import CopyTag from '../components/CopyTag.jsx';
 
+import { getMe, getTopArtists, getTopTracks, getRecentlyPlayed } from "../lib/spotify/api.js";
+
 function Fetch() {
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        //Spotify Data
+        const me = await getMe();
+        const topArtistsRes = await getTopArtists(10);
+        const topTracksRes = await getTopTracks(10);
+        const recentRes = await getRecentlyPlayed(10);
+
+
+        //Normalise into format for Gemini endpoints
+        const profile = {
+          displayName: me.display_name,
+          topArtists: topArtistsRes.items.map((a) => a.name),
+          topTracks: topTracksRes.items.map((t) => `${t.name} - ${t.artists?.[0]?.name ?? "Unknown"}`),
+          recentlyPlayed: recentRes.items.map((i) => `${i.track.name} - ${i.track.artists?.[0]?.name ?? "Uknown"}`),
+        };
+
+        //Send to backend Gemini endpoint
+        const res = await fetch("/api/roast", {
+          method: "POST",
+          headers: {"Content-Type" : "applications/json"},
+          body: JSON.stringify({ profile }),
+        });
+
+        if (!res.ok) {
+          const text = await res.text();
+          throw new Error(`Roast API failed: ${res.status} ${text}`);
+        }
+
+        const roast = await res.json();
+
+        //Save roast and go to roast page
+        sessionStorage.setItem("roast_payload", JSON.stringify(roast));
+        navigate("/roast", {replace: true});
+      } catch (e) {
+        navigate("/", {replace: true});
+      }
+    })();
+
+  }, [navigate]);
+
   return (
     <>
       <LogoComponent/>
@@ -18,7 +67,7 @@ function Fetch() {
       />
       <CopyTag text="We're Analyzing Your Recent Spotify Plays 🎶"/>
     </>
-  )
+  );
 }
 
 export default Fetch;
