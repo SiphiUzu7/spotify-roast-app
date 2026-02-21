c4ef2fca60833d3b03b125586f4501677d168883
Siphiwe Ngidi <mohlabaisiphiwe7@gmail.com>
2026-02-21 11:16:06 +0200

    Fix route mismatch

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { Routes, Route, Link } from 'react-router-dom';
-import { useState } from 'react'
 import './index.css'
 import Landing from './pages/Landing.jsx'
 import Fetch from './pages/Fetch.jsx'
 import Roast from './pages/Roast.jsx'
+import Callback from './pages/Callback.jsx'
 
 function App() {
 
@@ -11,8 +11,10 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Landing/>} />
-        <Route path="/fetching" element={<Fetch/>} />
-        <Route path="/roasted" element={<Roast/>} />
+        <Route path="/callback" element={<Callback/>}/>
+        <Route path="/fetch" element={<Fetch/>} />
+        <Route path="/roast" element={<Roast/>} />
+        <Route path="*" element={<Landing/>} />
       </Routes>
     </>
   )
